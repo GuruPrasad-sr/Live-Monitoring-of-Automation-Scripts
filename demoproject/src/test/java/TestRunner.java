8e86b3154b6db06f66bff4dab4013fff272ac966
Guruprasad <guruprasadsr10@gmail.com>
2023-05-31 13:53:08 +0530

    updated get request

--- demoproject/src/test/java/TestRunner.java
+++ demoproject/src/test/java/TestRunner.java
@@ -1,13 +1,11 @@
-package com.typicode.user;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/com/typicode/user/features",
-        glue = "com/typicode/user/steps",
+        features = "src/test/resource/features/user.feature",
+        glue = "src/test/java/com/typicode/user/steps",
         tags = "not @skip",
         plugin = {"pretty", "json:target/cucumber-report.json"},
         publish = true
